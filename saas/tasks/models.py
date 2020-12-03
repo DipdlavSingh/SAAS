7741eeae07bca512b03b9f4bb536adab71ac1586
DipdlavSingh <diplav@strongarmtech.com>
2020-12-04 02:29:17 +0530

    add user to task

--- saas/tasks/models.py
+++ saas/tasks/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from saas.users.models import User
 
 # Create your models here.
 
@@ -16,9 +18,10 @@ PRIORITIES = (
 )
 
 class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    assignee = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True)
+    assignee = models.ForeignKey(User, related_name="tasks", blank=True)
     status = models.CharField(
         choices=COMPLETION_STATUS, default=COMPLETION_STATUS[0][0], max_length=32)
     priority = models.CharField(
